72ce5a39c7603afd101859de7ab7f4a0bde8495f
LXMRTN <alex.marten@outlook.com>
2022-01-16 07:51:58 +0100

    Add JS file

---
+++ main.js
@@ -0,0 +1,3 @@
+const namesArr = ["Toby", "Michael", "Andy", "Jim", "Dwight", "Creed", "Angela", "Stanley", "Oscar", "Kevin"];
+const verbArr = ["likes", "enjoys", "loves", "digs", "dislikes", "hates", "detests", "resents", "rejects"];
+const objectArr = ["spring", "summer", "autumn", "winter"];
